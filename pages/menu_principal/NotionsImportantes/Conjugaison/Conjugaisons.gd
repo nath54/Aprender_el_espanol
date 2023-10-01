81fede1e51673f91edfdc24274f67ce47f9c6e61
nathan cerisara <cerisara.nathan@gmail.com>
2023-10-01 19:18:29 +0200

    Ajout des pages de conjugaisons du temps de l'indicatif

--- pages/menu_principal/NotionsImportantes/Conjugaison/Conjugaisons.gd
+++ pages/menu_principal/NotionsImportantes/Conjugaison/Conjugaisons.gd
@@ -19,80 +19,93 @@ func _on_bt_passe_compose_pressed():
 
 
 func _on_bt_imparfait_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://pages/menu_principal/NotionsImportantes/Conjugaison/Indicatif/Imparfait.tscn");
 
 
 func _on_bt_plusqueparfait_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://pages/menu_principal/NotionsImportantes/Conjugaison/Indicatif/PlusQueParfait.tscn");
 
 
 func _on_bt_passe_simple_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://pages/menu_principal/NotionsImportantes/Conjugaison/Indicatif/PasseSimple.tscn");
 
 
 func _on_bt_futur_simple_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://pages/menu_principal/NotionsImportantes/Conjugaison/Indicatif/FuturSimple.tscn");
 
 
 func _on_bt_futur_anterieur_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://pages/menu_principal/NotionsImportantes/Conjugaison/Indicatif/FuturAnterieur.tscn");
 
 
 func _on_bt_conditionnel_present_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://pages/menu_principal/NotionsImportantes/Conjugaison/Indicatif/ConditionnelPresent.tscn");
 
 
 func _on_bt_conditionnel_passe_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://pages/menu_principal/NotionsImportantes/Conjugaison/Indicatif/ConditionnelPasse.tscn");
 
 
 func _on_bt_present_subjonctif_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_passe_subonctif_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_imparfait_1_subjonctif_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_imparfait_2_subjonctif_2_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_plusqueparfait_1_subjonctif_3_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_plusqueparfait_2_subjonctif_4_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_futur_subjonctif_5_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_futur_anterieur_subjonctif_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_present_impératif_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_present_impératif_negatif_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_infinitif_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_gérondif_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
 
 
 func _on_bt_participe_regulier_pressed():
+	#get_tree().change_scene_to_file();
 	pass # Replace with function body.
